8cae11057c1b33deb9d34a7ef2b117fd9507d2a4
Kennerges <kennerges@gmail.com>
2020-08-13 14:44:03 -0300

    Project finished

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,53 @@
 package application;
 
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        List<Employee> employeeList = new ArrayList<>();
+
+        System.out.print("Enter the number of employees: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i ++) {
+            System.out.println("Employee #" + i + " data:");
+            System.out.print("Outsourced (y/n)? ");
+            char answer = scanner.next().charAt(0);
+            System.out.print("Name: ");
+            scanner.nextLine();
+            String name = scanner.nextLine();
+            System.out.print("Hours: ");
+            int hours = scanner.nextInt();
+            System.out.print("Value per hour: ");
+            double valuePerHour = scanner.nextDouble();
+
+            if (answer == 'y') {
+                System.out.print("Additional charge: ");
+                double additionalCharge = scanner.nextDouble();
+                employeeList.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+            } else {
+                employeeList.add(new Employee(name, hours, valuePerHour));
+            }
+        }
+
+        System.out.println();
+        System.out.println("PAYMENTS: ");
+        for (Employee emp : employeeList) {
+            System.out.println(emp);
+        }
+
+        scanner.close();
+
     }
 }
